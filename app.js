28c2ee8f34ce6f0b49da59757d17ac33453ccd15
Vanshika Saxena <145781723+Vanshika290@users.noreply.github.com>
2026-05-05 13:15:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+
 const session = require('express-session');
-const methodOverride = require('method-override');
 const User = require('./models/user');
 const userRoutes = require('./routes/userRoutes');
 const projectRoutes = require('./routes/projectRoutes');
@@ -13,35 +12,14 @@ const app = express();
 
 // Database Connection
 mongoose.connect('mongodb://127.0.0.1:27017/projectDB')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB Connection Error:', err));
+    
 
-// Settings
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Session Configuration
-const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-};
-app.use(session(sessionConfig));
-
-// Passport Configuration
 app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -51,13 +29,10 @@ app.use((req, res, next) => {
     next();
 });
 
-// Routes
+
 app.use('/', userRoutes);
 app.use('/', projectRoutes);
 
-app.get('/', (req, res) => {
-    res.redirect('/projects');
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
