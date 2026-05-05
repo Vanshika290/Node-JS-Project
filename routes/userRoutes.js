5a8a03ed6e91838d7bcec3c2720cceb08d61c579
Vanshika Saxena <145781723+Vanshika290@users.noreply.github.com>
2026-05-05 13:10:44 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,35 +7,10 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
-router.post('/register', async (req, res) => {
-    try {
-        const { username, password, fullName, profession } = req.body;
-        const user = new User({ username, fullName, profession });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            res.redirect('/projects');
-        });
-    } catch (e) {
-        console.log(e);
-        res.redirect('/register');
-    }
-});
 
 router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.post('/login', passport.authenticate('local', {
-    failureRedirect: '/login',
-    successRedirect: '/projects'
-}));
-
-router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-        if (err) { return next(err); }
-        res.redirect('/login');
-    });
-});
 
 module.exports = router;
